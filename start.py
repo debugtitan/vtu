91f028b954b82b90dd1569e8f890db4de31ba126
debugtitan <alts.devs@gmail.com>
2024-05-18 14:05:08 +0100

    import >> COnfigManager

--- start.py
+++ start.py
@@ -1,15 +1,24 @@
 import logging
 from telegram import Chat, User
 from telegram.ext import Application
+from mkconfig import ConfigManager
 
 log = logging.getLogger(__name__)
 
 
 class MirkBot:
 
-    def __init__(self, bot: Application, chat: Chat, user: User, conf, *args, **kwargs):
+    def __init__(
+        self,
+        bot: Application,
+        chat: Chat,
+        user: User,
+        conf: ConfigManager,
+        *args,
+        **kwargs
+    ):
         self.bot = bot
         self.chat = chat
         self.user = user
-        self.conf = None
+        self.conf = conf
         self.loc = None
