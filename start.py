23a46fdf7b538face350e4eaa3c88757d6454fd0
debugtitan <alts.devs@gmail.com>
2024-05-18 13:29:32 +0100

    >> bot setup

--- start.py
+++ start.py
@@ -1,6 +1,15 @@
-import telegram
+import logging
+from telegram import Chat, User
+from telegram.ext import Application
+
+log = logging.getLogger(__name__)
+
 
 class MirkBot:
-    
-    def __init__(self, bot, chat, user, args) -> None:
-        pass
+
+    def __init__(self, bot: Application, chat: Chat, user: User, conf, *args, **kwargs):
+        self.bot = bot
+        self.chat = chat
+        self.user = user
+        self.conf = None
+        self.loc = None
