3d7aad56957c90f583c57b3deb5295379a31de66
debugtitan <alts.devs@gmail.com>
2024-08-02 22:52:27 +0100

    imports start command

--- billbuddy/bot.py
+++ billbuddy/bot.py
@@ -4,6 +4,8 @@ from telegram.ext import Application, CommandHandler
 from billbuddy.utils import logger, exceptions
 from billbuddy import config
 
+from billbuddy.actions import start
+
 
 class BillBuddyBot:
     def __init__(self, token):
@@ -14,6 +16,9 @@ class BillBuddyBot:
 
     def run(self):
         logger.info("Bot start up!")
+
+        self.app.add_handler(CommandHandler("start", start.start_command_handler))
+
         self.app.run_polling()
 
     @staticmethod
