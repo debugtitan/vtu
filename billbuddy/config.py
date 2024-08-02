50d3a0db9fce405c51b718c70280ebf86e5d476b
debugtitan <alts.devs@gmail.com>
2024-08-02 22:52:38 +0100

    initialize dotenv

--- billbuddy/config.py
+++ billbuddy/config.py
@@ -1,6 +1,12 @@
 import os
+from pathlib import Path
+from dotenv import load_dotenv
 
-BOT_TOKEN: str = os.getenv("TOKEN","*****")
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-# Time wait in secs after each error 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+BOT_TOKEN: str = os.getenv("TOKEN", "*****")
+
+# Time wait in secs after each error
 TIMED_OUT_PAUSE: int = 15
