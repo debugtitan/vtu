89d8eacc97c79e73dff229771373ac6fd879cad8
debugtitan <alts.devs@gmail.com>
2024-08-02 23:52:09 +0100

    baseenums

---
+++ billbuddy/utils/enums.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+
+class BaseEnum(Enum):
+    @classmethod
+    def choices(cls):
+        return [(i.value, i.name) for i in cls]
+
+    @classmethod
+    def values(cls):
+        return list(i.value for i in cls)
+
+    @classmethod
+    def count(cls):
+        return len(cls)
+
+    @classmethod
+    def mapping(cls):
+        return dict((i.name, i.value) for i in cls)
+
+
+class ReactionType(BaseEnum):
+    INFO = "ℹ"
+    ERROR = "🚫"
+    PENDING = "⏳"
+    NOTIFICATION = "🔔"
+    WAVE = "👋"
+    DONE = "✅"
+    CANCEL = "❌"
+    PROCEED = "▶️"
