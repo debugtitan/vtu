d1c1bde0909bf6f781ef4566e58af325db082bf9
debugtitan <alts.devs@gmail.com>
2024-08-02 22:10:08 +0100

    billbuddy logger

---
+++ billbuddy/utils/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+# Start logging setup
+logger = logging.getLogger("app")
+
+# Set logging levels for specific loggers
+logging.getLogger("telegram").setLevel(logging.ERROR)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+# root logger
+logger.root.setLevel(logging.INFO)
+
+# console handler
+console_log = logging.StreamHandler()
+
+# message format
+format = "∴ %(filename)s: line %(lineno)s ∴ %(funcName)s ∴ %(message)s"
+console_log.setFormatter(logging.Formatter(format))
+
+
+logger.root.handlers.clear()
+logger.root.addHandler(console_log)
+
+# Log a debug message
+logger.debug("BillBuddy Logger")
