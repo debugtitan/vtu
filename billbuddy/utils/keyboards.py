e4dcb0c2d3632775c10d67d2ecaa0b910ff5b1df
debugtitan <alts.devs@gmail.com>
2024-08-04 07:53:29 +0100

    renamed

--- billbuddy/utils/keyboards.py
+++
@@ -1,8 +0,0 @@
-from telegram import KeyboardButton, ReplyKeyboardMarkup
-from billbuddy.utils import enums
-from billbuddy.utils import cfg
-
-
-def start_menu():
-    START_MENU = "CONTINUE " + enums.ReactionType.PROCEED.value
-    return ReplyKeyboardMarkup([[KeyboardButton(START_MENU)]], resize_keyboard=True)
