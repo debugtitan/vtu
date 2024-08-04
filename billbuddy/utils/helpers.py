8a57c6a20dd55639eddc864497865a8c94c898f3
debugtitan <alts.devs@gmail.com>
2024-08-04 09:58:57 +0100

    inline keyboard

--- billbuddy/utils/helpers.py
+++ billbuddy/utils/helpers.py
@@ -1,4 +1,5 @@
-from telegram import KeyboardButton
+from typing import Dict, List, Optional
+from telegram import KeyboardButton, InlineKeyboardButton
 
 
 def create_keyboard_button(
@@ -24,4 +25,50 @@ def create_keyboard_button(
         menu.insert(0, [KeyboardButton(button_header)])
     if button_footer:
         menu.append([KeyboardButton(_button) for _button in button_footer])
+    return menu
+
+
+def create_inline_button(
+    buttons: Dict[str, str],
+    buttons_per_column: int = 1,
+    button_header: Optional[Dict[str, str]] = None,
+    button_footer: Optional[Dict[str, str]] = None,
+    callback_text: Optional[str] = None,
+) -> List[List[InlineKeyboardButton]]:
+
+    menu = [
+        [
+            InlineKeyboardButton(
+                text, callback_data=callback_text + data if callback_text else data
+            )
+            for text, data in list(buttons.items())[i : i + buttons_per_column]
+        ]
+        for i in range(0, len(buttons), buttons_per_column)
+    ]
+
+    if button_header:
+        header_button = list(button_header.items())[0]
+        menu.insert(
+            0,
+            [
+                InlineKeyboardButton(
+                    header_button[0],
+                    callback_data=(
+                        callback_text + header_button[1]
+                        if callback_text
+                        else header_button[1]
+                    ),
+                )
+            ],
+        )
+
+    if button_footer:
+        footer_buttons = [
+            InlineKeyboardButton(
+                text, callback_data=callback_text + data if callback_text else data
+            )
+            for text, data in button_footer.items()
+        ]
+        menu.append(footer_buttons)
+
     return menu
