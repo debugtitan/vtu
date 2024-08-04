382a1a30fef321c05596d0530f0097d6c2aaccc4
debugtitan <alts.devs@gmail.com>
2024-08-04 07:01:50 +0100

    sql database url config

--- billbuddy/config.py
+++ billbuddy/config.py
@@ -10,3 +10,7 @@ BOT_TOKEN: str = os.getenv("TOKEN", "*****")
 
 # Time wait in secs after each error
 TIMED_OUT_PAUSE: int = 15
+
+
+# Database
+SQLALCHEMY_DATABASE_URL: str = os.getenv("SQLALCHEMY_DATABASE_URL", "*****")
