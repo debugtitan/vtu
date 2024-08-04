b0dfba8cab4d0b2a66ce125c24b7c0e84258c84e
debugtitan <alts.devs@gmail.com>
2024-08-04 07:37:04 +0100

    users model

---
+++ billbuddy/resources/users.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+from sqlalchemy import Column, String, Boolean, DECIMAL, Enum, Integer
+
+from billbuddy.resources.connection import Base
+from billbuddy.utils import enums
+
+
+class Users(enums.BaseModelMixin, Base):
+    """Default user models for billbuddy telegram app"""
+
+    first_name = Column(String)
+    last_name = Column(String, nullable=True)
+    username = Column(String, unique=True)
+    tg_id = Column(Integer, unique=True)
+    email = Column(String, nullable=True)
+    balance = Column(
+        DECIMAL(precision=20, scale=2), default=lambda: Decimal("0.00"), nullable=False
+    )
+    account_type = Column(
+        Enum(enums.AccountType), default=enums.AccountType.CLIENT.value, nullable=False
+    )
+    phone_number = Column(String, nullable=True)
+    wallet = Column(String, nullable=True)
+    is_restricted = Column(Boolean, default=False)
+
+    @property
+    def is_admin(self):
+        return self.account_type != enums.AccountType.CLIENT.value
+
+    @property
+    def is_owner(self):
+        return self.account_type == enums.AccountType.OWNER.value
