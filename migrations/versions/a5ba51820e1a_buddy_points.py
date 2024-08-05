fa6ef3d059da8c112f038b510a93c04d997f3ea6
debugtitan <alts.devs@gmail.com>
2024-08-05 20:26:35 +0100

    server default : str

--- migrations/versions/a5ba51820e1a_buddy_points.py
+++ migrations/versions/a5ba51820e1a_buddy_points.py
@@ -20,7 +20,10 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.add_column("users", sa.Column("buddy_points", sa.Integer(), default=0))
+    op.add_column(
+        "users",
+        sa.Column("buddy_points", sa.Integer(), nullable=False, server_default="0"),
+    )
 
 
 def downgrade() -> None:
