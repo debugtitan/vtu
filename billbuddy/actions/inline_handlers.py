38d5d40a5f1f129b4fc8cd76b88a2976b113460b
debugtitan <alts.devs@gmail.com>
2024-08-05 19:40:41 +0100

    use valid response decorators

--- billbuddy/actions/inline_handlers.py
+++ billbuddy/actions/inline_handlers.py
@@ -1,11 +1,14 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 from billbuddy.actions.menu import main_menu_handler
-from billbuddy.utils import enums
+from billbuddy.utils import enums, decorators
 from billbuddy.resources import dao
 
 
-async def language_handler(update: Update, callback: ContextTypes.DEFAULT_TYPE):
+@decorators.is_valid_response
+async def language_handler(
+    update: Update, callback: ContextTypes.DEFAULT_TYPE, *args, **kwargs
+):
     """handles the localization changes"""
     query = update.callback_query
     user = query.from_user
