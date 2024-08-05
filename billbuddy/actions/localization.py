f5aabadd40f25a29950083a775a0007a5f1394fa
debugtitan <alts.devs@gmail.com>
2024-08-05 19:41:23 +0100

    decorator and user kwargs to fetch user language

--- billbuddy/actions/localization.py
+++ billbuddy/actions/localization.py
@@ -1,13 +1,18 @@
 from telegram import Update, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
-from billbuddy.utils import cfg, helpers, enums
+from billbuddy.utils import cfg, helpers, enums, decorators
 
 
-async def localization_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard_template: list = cfg("start.toml").get("KEYBOARDS")["en"]
+@decorators.is_valid_response
+async def localization_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs
+):
+    lang = kwargs.get("lang", "en")
+    keyboard_template: list = cfg("start.toml").get("KEYBOARDS")[lang]
+    msg = cfg("start.toml").get("LANGUAGE_MESSAGE")[lang]
     await update.message.reply_text(
-        f"{enums.ReactionType.INFO.value} please choose your preferred language",
+        msg,
         parse_mode=ParseMode.MARKDOWN,
         reply_markup=InlineKeyboardMarkup(
             helpers.create_inline_button(
