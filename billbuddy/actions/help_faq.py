4b19c0fca1cec21358bb0a0071db942e5a5ad1e1
debugtitan <alts.devs@gmail.com>
2024-08-05 19:40:19 +0100

    help handler

--- billbuddy/actions/help_faq.py
+++ billbuddy/actions/help_faq.py
@@ -1,3 +1,12 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from telegram.constants import ParseMode
 
-async def help_message_handler():
+from billbuddy.utils import decorators
+
+
+@decorators.is_valid_response
+async def help_message_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs
+):
     """"""
