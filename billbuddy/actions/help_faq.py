c7e285da259fd71985d4e64f57c5da8da2f6a5d6
debugtitan <alts.devs@gmail.com>
2024-08-05 19:49:22 +0100

    help message implementation

--- billbuddy/actions/help_faq.py
+++ billbuddy/actions/help_faq.py
@@ -2,11 +2,13 @@ from telegram import Update
 from telegram.ext import ContextTypes
 from telegram.constants import ParseMode
 
-from billbuddy.utils import decorators
+from billbuddy.utils import decorators, cfg
 
 
 @decorators.is_valid_response
 async def help_message_handler(
     update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs
 ):
-    """"""
+    lang = kwargs.get("lang", "en")
+    msg_template: str = cfg("help.toml").get("HELP")[lang]
+    await update.message.reply_text(msg_template, parse_mode=ParseMode.MARKDOWN)
