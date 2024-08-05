5b16b188f223de89c5712815c9dcf0a9865d216c
debugtitan <alts.devs@gmail.com>
2024-08-05 19:42:04 +0100

    remove function getting user id, and use decoratoers and kwargs to fetch user id and language code

--- billbuddy/actions/menu.py
+++ billbuddy/actions/menu.py
@@ -2,20 +2,19 @@ from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import ContextTypes
 
 from billbuddy.resources.dao.users import get_user
-from billbuddy.utils import cfg, helpers
+from billbuddy.utils import cfg, helpers, decorators
 
 
-async def main_menu_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+@decorators.is_valid_response
+async def main_menu_handler(
+    update: Update, context: ContextTypes.DEFAULT_TYPE, *args, **kwargs
+):
     """main menu for bill point"""
-    try:
-        user_id = update.message.from_user.id
-    except AttributeError as err:
-        user_id = update.callback_query.from_user.id
-    user = await get_user(user_id) or await get_user(user_id)
-    button = cfg("menu.toml").get("KEYBOARDS")[user.language_code]
-    msg_template = cfg("menu.toml").get("MESSAGE")[user.language_code]
+    user_lang = kwargs.get("lang", "en")
+    button = cfg("menu.toml").get("KEYBOARDS")[user_lang]
+    msg_template = cfg("menu.toml").get("MESSAGE")[user_lang]
     await context.bot.send_message(
-        user.tg_id,
+        kwargs.get("id"),
         msg_template,
         reply_markup=ReplyKeyboardMarkup(
             helpers.create_keyboard_button(
