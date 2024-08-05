f0f463911bf23fbc232a2a9a0f2a573d623aad19
debugtitan <alts.devs@gmail.com>
2024-08-05 19:40:58 +0100

    decorator on all function

--- billbuddy/actions/keyboard_handlers.py
+++ billbuddy/actions/keyboard_handlers.py
@@ -1,9 +1,11 @@
 from telegram.ext import ContextTypes
 from telegram import Update
 
+from billbuddy.utils import decorators
 
+@decorators.is_valid_response
 async def language_selection_handler(
-    update: Update, callback: ContextTypes.DEFAULT_TYPE
+    update: Update, callback: ContextTypes.DEFAULT_TYPE, *args, **kwargs
 ):
     """handle any type of language selected"""
     print(update.message.text)
